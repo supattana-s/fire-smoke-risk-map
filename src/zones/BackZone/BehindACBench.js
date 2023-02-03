4fdd981aaea22a1801ec72f6265e51d1f0143760
supattana-s <supattana.sue@gmail.com>
2023-02-03 10:19:45 +0700

    Done BackZone plug, modal include data from db

--- src/zones/BackZone/BehindACBench.js
+++ src/zones/BackZone/BehindACBench.js
@@ -1,18 +1,30 @@
 import ACMainPower from "../../component/ACMainPower";
 import AirCompressor from "../../component/AirCompressor";
 import Cabinet from "../../component/Cabinet";
+import Plug from "../../component/Plug";
+import { useModal } from "../../contexts/ModalContext";
 
 function BehindACBench() {
+    const { handleOpenModal } = useModal();
+
     return (
         <div className="d-flex flex-row w-100">
+            <div className="plug-behind-ac-bench">
+                <Plug openModal={() => handleOpenModal("7")} />
+            </div>
             <div className="d-flex flex-row air-compressor-position w-70">
                 <AirCompressor />
                 <Cabinet />
                 <Cabinet />
                 <Cabinet />
             </div>
-            <div className="ac-behind-power-position w-30">
-                <ACMainPower behindAcBench={true} />
+            <div className="w-30">
+                <div className="breaker-ac-bench-behind">
+                    <Plug openModal={() => handleOpenModal("10")} />
+                </div>
+                <div className="ac-behind-power-position w-10">
+                    <ACMainPower behindAcBench={true} />
+                </div>
             </div>
         </div>
     );
